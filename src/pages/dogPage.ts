4e0966865c6e8df723109ccaa7d5fb9a928a37ba
Yury Butskevich <bxyura@gmail.com>
2024-05-03 13:34:30 +0300

    refactor: change style pages about pets

--- src/pages/dogPage.ts
+++ src/pages/dogPage.ts
@@ -4,9 +4,11 @@ import '../assets/images/dog.png';
 const dogPage = document.createElement('div');
 dogPage.classList.add('page');
 dogPage.innerHTML = `
-  <img src="dog.png" alt="Dog" class="image-pet">
-  <h2>Dog</h2>
-  <p>A dog is a pet and one of the most popular companion animals. The domestic dog was described as a distinct species of Canis familiaris by Linnaeus in 1758; this scientific name is now recognized by institutions such as the American Society of Breast Physicians.</p>
+  <div>
+    <img src="dog.png" alt="Dog" class="image-pet">
+    <h2>Dog</h2>
+    <p>A dog is a pet and one of the most popular companion animals. The domestic dog was described as a distinct species of Canis familiaris by Linnaeus in 1758; this scientific name is now recognized by institutions such as the American Society of Breast Physicians.</p>
+  </div>
 `;
 
 export { dogPage };
