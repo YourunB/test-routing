6d6047046fdc71df1f37f0f40376369dcbca1a0d
Yury Butskevich <bxyura@gmail.com>
2024-05-03 15:43:01 +0300

    feat: add Router class for work routing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import './index.sass';
-//import { catPage } from './pages/catPage';
-//import { dogPage } from './pages/dogPage';
+import { catPage } from './pages/catPage';
+import { dogPage } from './pages/dogPage';
 //import { hamsterPage } from './pages/hamsterPage';
 //import { rabbitPage } from './pages/rabbitPage';
 //import { raccoonPage } from './pages/raccoonPage';
-import { mainPage } from './pages/mainPage';
+//import { mainPage } from './pages/mainPage';
 
 const header = document.createElement('header');
 const main = document.createElement('main');
@@ -13,15 +13,13 @@ const footer = document.createElement('footer');
 header.innerHTML = `
   <ul>
     <li><a href="">Main</a></li>
-    <li><a href="">Cat</a></li>
-    <li><a href="">Dog</a></li>
+    <li><a href="/cat">Cat</a></li>
+    <li><a href="/dog">Dog</a></li>
     <li><a href="">Hamster</a></li>
     <li><a href="">Rabbit</a></li>
     <li><a href="">Raccoon</a></li>
   </ul>
 `;
-
-main.append(mainPage);
 
 footer.innerHTML = `
   <span>2024</span>
@@ -29,3 +27,62 @@ footer.innerHTML = `
 `;
 
 document.body.append(header, main, footer);
+
+type Route = {
+  path: string;
+  handler: () => void;
+};
+
+class Router {
+  private routes: Route[] = [];
+
+  constructor() {
+    window.addEventListener('popstate', () => {
+      this.route(window.location.pathname);
+    });
+  }
+
+  addRoute(route: Route) {
+    this.routes.push(route);
+  }
+
+  route(pathname: string) {
+    for (const route of this.routes) {
+      if (route.path === pathname) {
+        window.history.pushState({}, '', pathname);
+        route.handler();
+        return;
+      }
+    }
+    console.log(`No handler found for ${pathname}`);
+  }
+}
+
+// Использование:
+const router = new Router();
+
+router.addRoute({
+  path: '/cat',
+  handler: () => {
+    main.innerHTML = '';
+    main.append(catPage);
+    console.log('Cat page');
+  },
+});
+
+router.addRoute({
+  path: '/dog',
+  handler: () => {
+    main.innerHTML = '';
+    main.append(dogPage);
+    console.log('Dog page');
+  },
+});
+
+header.addEventListener('click', () => {
+  router.route('/dog');
+});
+
+footer.addEventListener('click', () => {
+  router.route('/cat');
+});
