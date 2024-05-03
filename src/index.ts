06375f39732a89aadfeb4cb66587f0a89fd8e7c8
Yury Butskevich <bxyura@gmail.com>
2024-05-03 18:56:48 +0300

    feat: add all roter link for all menu items

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 import './index.sass';
 import { catPage } from './pages/catPage';
 import { dogPage } from './pages/dogPage';
-//import { hamsterPage } from './pages/hamsterPage';
-//import { rabbitPage } from './pages/rabbitPage';
-//import { raccoonPage } from './pages/raccoonPage';
-//import { mainPage } from './pages/mainPage';
+import { hamsterPage } from './pages/hamsterPage';
+import { rabbitPage } from './pages/rabbitPage';
+import { raccoonPage } from './pages/raccoonPage';
+import { mainPage } from './pages/mainPage';
 
 const header = document.createElement('header');
 const main = document.createElement('main');
 const footer = document.createElement('footer');
 
 header.innerHTML = `
-  <ul>
-    <li><a href="">Main</a></li>
-    <li><a href="/cat">Cat</a></li>
-    <li><a href="/dog">Dog</a></li>
-    <li><a href="">Hamster</a></li>
-    <li><a href="">Rabbit</a></li>
-    <li><a href="">Raccoon</a></li>
+  <ul class="menu">
+    <li class="menu__item">Main</li>
+    <li class="menu__item">Cat</li>
+    <li class="menu__item">Dog</li>
+    <li class="menu__item">Hamster</li>
+    <li class="menu__item">Rabbit</li>
+    <li class="menu__item">Raccoon</li>
   </ul>
 `;
 
@@ -58,15 +58,21 @@ class Router {
   }
 }
 
-// Использование:
 const router = new Router();
+
+router.addRoute({
+  path: '/test-routing/',
+  handler: () => {
+    main.innerHTML = '';
+    main.append(mainPage);
+  },
+});
 
 router.addRoute({
   path: '/test-routing/cat',
   handler: () => {
     main.innerHTML = '';
     main.append(catPage);
-    console.log('Cat page');
   },
 });
 
@@ -75,14 +81,49 @@ router.addRoute({
   handler: () => {
     main.innerHTML = '';
     main.append(dogPage);
-    console.log('Dog page');
   },
 });
 
-header.addEventListener('click', () => {
-  router.route('/test-routing/dog');
+router.addRoute({
+  path: '/test-routing/hamster',
+  handler: () => {
+    main.innerHTML = '';
+    main.append(hamsterPage);
+  },
 });
 
-footer.addEventListener('click', () => {
+router.addRoute({
+  path: '/test-routing/rabbit',
+  handler: () => {
+    main.innerHTML = '';
+    main.append(rabbitPage);
+  },
+});
+
+router.addRoute({
+  path: '/test-routing/raccoon',
+  handler: () => {
+    main.innerHTML = '';
+    main.append(raccoonPage);
+  },
+});
+
+const menuItems = document.getElementsByClassName('menu__item');
+menuItems[0].addEventListener('click', () => {
+  router.route('/test-routing/');
+});
+menuItems[1].addEventListener('click', () => {
   router.route('/test-routing/cat');
+});
+menuItems[2].addEventListener('click', () => {
+  router.route('/test-routing/dog');
+});
+menuItems[3].addEventListener('click', () => {
+  router.route('/test-routing/hamster');
+});
+menuItems[4].addEventListener('click', () => {
+  router.route('/test-routing/rabbit');
+});
+menuItems[5].addEventListener('click', () => {
+  router.route('/test-routing/raccoon');
 });
