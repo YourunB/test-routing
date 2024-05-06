c6835d82120df9dacb497c5284dc50f662c1eb4e
Yury Butskevich <bxyura@gmail.com>
2024-05-06 13:24:35 +0300

    feat: add image and animation for 404 page

--- src/pages/notFoundPage.ts
+++ src/pages/notFoundPage.ts
@@ -1,10 +1,14 @@
 import './notFoundPage.sass';
+import '../assets/images/earth.svg';
 
 const notFoundPage = document.createElement('div');
 notFoundPage.classList.add('page-404');
 notFoundPage.innerHTML = `
-  <h2 class="page-404__title">404</h2>
-  <p class="page-404__description">This Page Not Found</p>
+  <div>
+    <h2 class="page-404__title">404</h2>
+    <img class="page-404__image" src="earth.svg"> 
+    <p class="page-404__description">This Page Not Found</p>
+  </div>
 `;
 
 export { notFoundPage };
